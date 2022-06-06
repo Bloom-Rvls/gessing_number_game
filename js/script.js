746ec72bd440368520b093219c37d2734a0f6785
bloom <angelicaravelonjohanison@gmail.com>
2022-06-06 19:08:29 +0100

    version avec essais première amelioration

--- js/script.js
+++ js/script.js
@@ -6,8 +6,9 @@ let randomNumber = Math.floor(Math.random() * 10) + 1;
 let nbrEssais = 0;
 
 
-    for (let i=0; i<allBoutonLink.length; i++) { //pour acceder aux contenus du tableau il faut parcourir une boucle
-        allBoutonLink[i].addEventListener ("click", () => {
+for (let i=0; i<allBoutonLink.length; i++) { //pour acceder aux contenus du tableau il faut parcourir une boucle
+    allBoutonLink[i].addEventListener ("click", () => {            
+        if (nbrEssais <= 3) {
             if(allBoutonLink[i].textContent == randomNumber) {
                 allBoutonLink[i].getAttribute('href');
                 allBoutonLink[i].setAttribute('href','congrats.html');
@@ -17,17 +18,41 @@ let nbrEssais = 0;
                     */
             } else {
                 do {
-                    if(nbrEssais <= 3) {
-                        statut.innerHTML = "Il vous reste : " + nbrEssais + " chances";
-                    } else {
-                        allBoutonLink[i].getAttribute('href');
-                        allBoutonLink[i].setAttribute('href','game over.html');
-                    }
+                    statut.innerHTML = "Il vous reste : " + nbrEssais + " chances";
                     nbrEssais++;
                 } while(nbrEssais < 3);
             }
-        });
-    }
+        } else {
+            if(allBoutonLink[i].textContent == randomNumber) {
+                allBoutonLink[i].getAttribute('href');
+                allBoutonLink[i].setAttribute('href','congrats.html');
+            } else {                    
+                allBoutonLink[i].getAttribute('href');
+                allBoutonLink[i].setAttribute('href','game over.html');
+            }                       
+        }
+
+
+
+            /**
+                if(allBoutonLink[i].textContent == randomNumber) {
+                allBoutonLink[i].getAttribute('href');
+                allBoutonLink[i].setAttribute('href','congrats.html');  
+                } else {
+                    do {
+                        if(nbrEssais <= 3) {
+                            statut.innerHTML = "Il vous reste : " + nbrEssais + " chances";
+                        } else {
+                            allBoutonLink[i].getAttribute('href');
+                            allBoutonLink[i].setAttribute('href','game over.html');
+                        }
+                        nbrEssais++;
+                    } while(nbrEssais < 3);
+                }
+             */
+            
+    });
+}
 
 
 
