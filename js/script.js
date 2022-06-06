f59b67c0385e9b5705bdb3d0e68a8c3403031794
bloom <angelicaravelonjohanison@gmail.com>
2022-06-06 12:16:52 +0100

    version avec 3 esssais pas tout a fait finis

--- js/script.js
+++ js/script.js
@@ -1,26 +1,35 @@
 /* */
 let allBoutonLink = document.getElementsByTagName ("a");
+let statut = document.getElementById('statut');
 //generation d'un nombre aleatoire
 let randomNumber = Math.floor(Math.random() * 10) + 1;
-
+let nbrEssais = 0;
 
 
     for (let i=0; i<allBoutonLink.length; i++) { //pour acceder aux contenus du tableau il faut parcourir une boucle
         allBoutonLink[i].addEventListener ("click", () => {
-            if(allBoutonLink[i].textContent === randomNumber) {
+            if(allBoutonLink[i].textContent == randomNumber) {
                 allBoutonLink[i].getAttribute('href');
                 allBoutonLink[i].setAttribute('href','congrats.html');
-                /*
-                    on peut l'ecrire également comme ceci
-                    allBoutonLink[i].href = 'congrats.html';
-                */ 
+                    /*
+                        on peut l'ecrire également comme ceci
+                        allBoutonLink[i].href = 'congrats.html';
+                    */
             } else {
-                allBoutonLink[i].getAttribute('href');
-                allBoutonLink[i].setAttribute('href','game over.html');
+                do {
+                    if(nbrEssais <= 3) {
+                        statut.innerHTML = "Il vous reste : " + nbrEssais + " chances";
+                    } else {
+                        allBoutonLink[i].getAttribute('href');
+                        allBoutonLink[i].setAttribute('href','game over.html');
+                    }
+                    nbrEssais++;
+                } while(nbrEssais < 3);
             }
-
         });
     }
+
+
 
 
 
